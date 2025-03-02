15ee8111abcac9d7c917f8bc70d35ed835942890
Anastasia-Hlystova <m2408317@edu.misis.ru>
2025-03-02 23:44:05 +0300

    Update Purple_5.cs

--- Purple_5.cs
+++ Purple_5.cs
@@ -16,13 +16,7 @@ namespace Lab_6
             public string Animal => _animal;
             public string CharacterTrait => _charactertrait;
             public string Concept => _concept;
-            private string[] Answers
-            {
-                get
-                {
-                    return new string[] {_animal, _charactertrait, _concept};
-                }
-            }
+
             public Response(string animal, string charactertrait, string concept)
             {
                 _animal = animal;
@@ -31,14 +25,23 @@ namespace Lab_6
             }
             public int CountVotes(Response[] responses, int questionNumber)
             {
-                if (responses == null || questionNumber < 1 || questionNumber > 3) return 0;
-                int count = 0;
-                int ind = questionNumber - 1;
-                foreach (var x in responses)
+                if (responses == null || responses.Length == 0 || questionNumber < 1 || questionNumber > 3) return 0;
+                if (questionNumber == 1)
                 {
-                    if (x.Answers[ind] != "") count++;
+                    return responses.Count(x => x.Animal != null && x.Animal.Length != 0);
                 }
-                return count;
+                else if (questionNumber == 2)
+                {
+                    return responses.Count(x => x.CharacterTrait != null && x.CharacterTrait.Length != 0);
+                }
+                else if (questionNumber == 3)
+                {
+                    return responses.Count(x => x.Concept != null && x.Concept.Length != 0);
+                }
+                else
+                {
+                    return 0;
+                }
             }
             public void Print()
             {
@@ -66,14 +69,15 @@ namespace Lab_6
             }
             public void Add(string[] answers)
             {
-                if(_responses == null || answers == null) return;
-                var result = new Response[_responses.Length + 1];
-                Array.Copy(_responses, result, result.Length-1);
+                if(_responses == null || answers == null || answers.Length != 3) return;
+                
                 string[] ans = new string[] {null, null, null};
                 for (int i = 0; i < Math.Min(3, answers.Length); i++)
                 {
                     ans[i] = answers[i];
                 }
+                var result = new Response[_responses.Length + 1];
+                Array.Copy(_responses, result, result.Length-1);
                 Response R = new Response(ans[0], ans[1], ans[2]);
                 result[result.Length-1] = R;
                 _responses = result;
@@ -83,15 +87,15 @@ namespace Lab_6
                 if (_responses == null || question < 1 || question > 3) { return null; }
                 if (question == 1)
                 {
-                    return _responses.GroupBy(x => x.Animal).Where(x => x.Key != null && x.Key.Length != null).OrderByDescending(x => x.Count()).Take(5).Select(x => x.Key).ToArray();
+                    return _responses.GroupBy(x => x.Animal).Where(x => x.Key != null && x.Key.Length != 0).OrderByDescending(x => x.Count()).Take(5).Select(x => x.Key).ToArray();
                 }
                 else if (question == 2)
                 {
-                    return _responses.GroupBy(x => x.CharacterTrait).Where(x => x.Key != null && x.Key.Length != null).OrderByDescending(x => x.Count()).Take(5).Select(x => x.Key).ToArray();
+                    return _responses.GroupBy(x => x.CharacterTrait).Where(x => x.Key != null && x.Key.Length != 0).OrderByDescending(x => x.Count()).Take(5).Select(x => x.Key).ToArray();
                 }
                 else if (question == 3)
                 {
-                    return _responses.GroupBy(x => x.Concept).Where(x => x.Key != null && x.Key.Length != null).OrderByDescending(x => x.Count()).Take(5).Select(x => x.Key).ToArray();
+                    return _responses.GroupBy(x => x.Concept).Where(x => x.Key != null && x.Key.Length != 0).OrderByDescending(x => x.Count()).Take(5).Select(x => x.Key).ToArray();
                 }
                 else
                 {
